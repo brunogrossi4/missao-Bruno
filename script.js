1357fc618ffb9d213e230495b3c11aaa7cbf03c8
brunogrossi4 <105368343+brunogrossi4@users.noreply.github.com>
2024-09-05 09:09:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-pergunta");
-const caixaAlternativa = document.querySelector(".caixa-alternativa");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+const caixaPrincipal document.querySelector(".caixa-principal");
 
-const perguntas = [
-    { //primeiro objeto da lista de perguntas
-        enunciado: "pergunta1",
-        alternativas: ["alternativa1", "alternativa2"] 
-    },
-    { //segundo objeto da lista de perguntas
-        enunciado: "pergunta2",
-        alternativas: ["alternativa1", "alternativa2"] 
-    }
-]
+const caixa Pergunta document.querySelector(".caixa-porguntas"); const caixaAlternativas document.querySelector(".caixa-alternativas");
+
+const caixaResultado const textoResultado document.querySelector(".caixa-resultado"); document.querySelector("testo-resultado");
+
+const perguntas T
+
+enunciado: "Como você acredita que podemos combater sociedade?", Tacismo
+
+alternativas: [
+
+texto: "Participando de campanhas de conscientização educação sobre igualdade racial." afirmacao: "afirmacaol"
+
+afirmacao: "afirmacao2"
+
+1
+
+1
+
+texto: "Denunciando atos de discriminação apoiando politicas públicas inclusivas.",
+
+enunciado: "Como você pode ajudar combater a disseminação de fake news, especialmente as que perpetuan o racismo?",
+
+alternativas: [
+
+texto: "Verificando a veracidade das informações antes de compartilhar qualquer conteúdo, especialmente sobre temas aensiveis como racismo.".
+
+afirmacao: "afirmacao3"
+
+texto: "Educando amigos e familiares sobre os perigos das incentivando-os racismo.", não compartilhar conteúdos falsos que afirmacao: "afirmacao4" perpetuan
+
+fake news
+
+1
+
+enunciados "Como podemos incentivar o uso de energia solar e outras formas de energia limpa, especialmente en comunidades carentes?", alternativas:
+
+(
+
+texto: "Defendendo politicas públicas que subsidion energia solar para familias de baixa renda.", afirmacao: "afirmacao5"
+
+escolas texto: "Promovendo a instalação de painéis solares em centros comunitários de comunidades marginalizadas.", afirmacao: "afirmacao6
+
+12
